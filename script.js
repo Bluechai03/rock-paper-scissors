96918ce508edea552404906e9376ac4fb5485963
Anna Montero <62479167+Bluechai03@users.noreply.github.com>
2021-07-15 22:09:04 +0400

    Organize layout and add computer side

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-// Scores
-let playerScore = 0;
-let computerScore = 0;
-const divPlayerScore = document.querySelector('#player--score');
-const divComputerScore = document.querySelector('#computer--score');
-const divRoundWinner = document.querySelector('#round-winner');
+function setItemButtons(btn, item) {
+  btn.addEventListener('click', () => {
+    playerSelection = item;
+
+    // Enable all item buttons
+    playerItems.forEach((playerItem) => (playerItem.disabled = false));
+    playerItems.disabled = false;
+    playerItems.textContent = 'boop';
+
+    // Disable selected button and add it's image to frame
+    btnPlayRound.disabled = false;
+    playerSelectionImg.src = `../images/${item}.png`;
+
+    btn.disabled = true;
+
+    const playerFrame = document.querySelector('#player--frame');
+    playerFrame.appendChild(playerSelectionImg);
+  });
+}
 
 // Write a function that randomly returns rock, Wool, shears as the computer's selection.
 function computerPlay() {
@@ -12,13 +25,14 @@ function computerPlay() {
   return shapes[Math.floor(Math.random() * shapes.length)];
 }
 
-const divPlayerSelection = document.querySelector('#player--selection');
-const divComputerSelection = document.querySelector('#computer--selection');
-const result = document.querySelector('#result');
-
+function setRoundWinner(roundWinner) {
+  divRoundWinner.textContent = `${roundWinner} wins a point!`;
+}
 // Write a function that takes the player's and computer's selection as parameters.
 // calculate the winner and return a string that declares the winner.
 function playRound(playerSelection, computerSelection) {
+  playerSelectionImg.src = '';
+  btnPlayRound.disabled = true;
   divPlayerSelection.textContent = `Player's hand: ${playerSelection.toUpperCase()}`;
   divComputerSelection.textContent = `Computer's hand: ${computerSelection.toUpperCase()}`;
 
@@ -68,6 +82,8 @@ function playRound(playerSelection, computerSelection) {
     divRoundWinner.textContent = "It's a tie!";
   }
 
+  updateScore();
+
   // End round once any score reaches 5
   if (playerScore >= 5 || computerScore >= 5) {
     playerScore >= 5 ? alert('Player wins!') : alert('Computer wins!');
@@ -75,13 +91,8 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function setRoundWinner(roundWinner) {
-  divRoundWinner.textContent = `${roundWinner} wins a point!`;
-}
-
 function resetGame() {
-  playerScore = 0;
-  computerScore = 0;
+  resetScore();
   divPlayerScore.textContent = "Player's Score: 0";
   divComputerScore.textContent = "Computer's Score: 0";
   divPlayerSelection.textContent = '';
@@ -91,7 +102,45 @@ function resetGame() {
   btnPlayRound.disabled = true;
 }
 
+function resetScore() {
+  playerScore = 0;
+  computerScore = 0;
+  playerScoreImg.forEach((score) => (score.src = '../images/heart.png'));
+}
+
+function updateScore() {
+  console.log('update score');
+  // Update score for computer
+  for (let i = 0; i < playerScore; i++) {
+    console.log(i);
+    computerScoreImg[i].src = '../images/empty-heart.png';
+  }
+
+  // Update score for player
+  for (let i = 0; i < computerScore; i++) {
+    playerScoreImg[i].src = '../images/empty-heart.png';
+  }
+}
+
+// Scores
+let playerScore = 0;
+let computerScore = 0;
+
+const playerScoreImg = document.querySelectorAll('.score--player');
+const computerScoreImg = document.querySelectorAll('.score--computer');
+
+const divPlayerScore = document.querySelector('#player--score');
+const divComputerScore = document.querySelector('#computer--score');
+const divRoundWinner = document.querySelector('#round-winner');
+
 let playerSelection;
+const divPlayerSelection = document.querySelector('#player--selection');
+const divComputerSelection = document.querySelector('#computer--selection');
+const result = document.querySelector('#result');
+
+const btnRock = document.querySelector('#btn--rock');
+const btnWool = document.querySelector('#btn--wool');
+const btnShears = document.querySelector('#btn--shears');
 
 const btnPlayRound = document.querySelector('#btn--play-round');
 btnPlayRound.disabled = true;
@@ -104,10 +153,6 @@ btnPlayRound.addEventListener('click', () => {
   }
 });
 
-const btnRock = document.querySelector('#btn--rock');
-const btnWool = document.querySelector('#btn--wool');
-const btnShears = document.querySelector('#btn--shears');
-
 const btnItems = [btnRock, btnWool, btnShears];
 const items = ['rock', 'wool', 'shears'];
 const playerSelectionImg = document.createElement('img');
@@ -117,23 +162,4 @@ for (let i = 0; i < items.length; i++) {
   setItemButtons(btnItems[i], items[i]);
 }
 
-function setItemButtons(btn, item) {
-  btn.addEventListener('click', () => {
-    playerSelection = item;
-
-    // Enable all item buttons
-    const playerItems = document.querySelectorAll('.player--item');
-    playerItems.forEach((playerItem) => (playerItem.disabled = false));
-    playerItems.disabled = false;
-    playerItems.textContent = 'boop';
-
-    // Disable selected button and add it's image to frame
-    btnPlayRound.disabled = false;
-    playerSelectionImg.src = `../images/${item}.png`;
-
-    btn.disabled = true;
-
-    const playerFrame = document.querySelector('#player--frame');
-    playerFrame.appendChild(playerSelectionImg);
-  });
-}
+const playerItems = document.querySelectorAll('.items__btn--player');
