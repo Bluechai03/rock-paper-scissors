dac2bad5cb6454f40679600872bdefaab51decbb
Anna Montero <62479167+Bluechai03@users.noreply.github.com>
2021-07-15 11:54:32 +0400

    Refactor logic to display results in page

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
 // Scores
 let playerScore = 0;
 let computerScore = 0;
-const divPlayerScore = document.querySelector("#player--score");
-const divComputerScore = document.querySelector("#computer--score");
-
-const divRoundWinner = document.querySelector("#round-winner");
-
-// Write a function to enter player's selection
-function playerPlay() {
-  let playerSelection = prompt("Enter a shape: ");
-  if (playerSelection === "ROCK" || "PAPER" || "SCISSORS") {
-    return playerSelection;
-  } else {
-    return null;
-  }
-}
+const divPlayerScore = document.querySelector('#player--score');
+const divComputerScore = document.querySelector('#computer--score');
+const divRoundWinner = document.querySelector('#round-winner');
 
 // Write a function that randomly returns rock, paper, scissors as the computer's selection.
 function computerPlay() {
   // Return rock, paper or scissors
-  let shapes = ["Rock", "Paper", "Scissors"];
+  let shapes = ['Rock', 'Paper', 'Scissors'];
   return shapes[Math.floor(Math.random() * shapes.length)];
 }
 
-const divPlayerSelection = document.querySelector("#player--selection");
-const divComputerSelection = document.querySelector("#computer--selection");
-const result = document.querySelector("#result");
+const divPlayerSelection = document.querySelector('#player--selection');
+const divComputerSelection = document.querySelector('#computer--selection');
+const result = document.querySelector('#result');
 
 // Write a function that takes the player's and computer's selection as parameters.
 // calculate the winner and return a string that declares the winner.
@@ -35,40 +24,40 @@ function playRound(playerSelection, computerSelection) {
 
   //   Conditional statements to check the winner
   //   If player chooses rock
-  if (playerSelection.toUpperCase() === "ROCK") {
-    if (computerSelection.toUpperCase() === "SCISSORS") {
-      result.textContent = "Rock beats scissors.";
-      setRoundWinner("Player");
+  if (playerSelection.toUpperCase() === 'ROCK') {
+    if (computerSelection.toUpperCase() === 'SCISSORS') {
+      result.textContent = 'Rock beats scissors.';
+      setRoundWinner('Player');
       divPlayerScore.textContent = `Player's Score: ${++playerScore}`;
-    } else if (computerSelection.toUpperCase() === "PAPER") {
-      result.textContent = "Paper beats rock.";
-      setRoundWinner("Computer");
+    } else if (computerSelection.toUpperCase() === 'PAPER') {
+      result.textContent = 'Paper beats rock.';
+      setRoundWinner('Computer');
       divComputerScore.textContent = `Computer's Score: ${++computerScore}`;
     }
   }
 
   //   If player chooses paper
-  else if (playerSelection.toUpperCase() === "PAPER") {
-    if (computerSelection.toUpperCase() === "ROCK") {
-      result.textContent = "Paper beats rock.";
-      setRoundWinner("Player");
+  else if (playerSelection.toUpperCase() === 'PAPER') {
+    if (computerSelection.toUpperCase() === 'ROCK') {
+      result.textContent = 'Paper beats rock.';
+      setRoundWinner('Player');
       divPlayerScore.textContent = `Player's Score: ${++playerScore}`;
-    } else if (computerSelection.toUpperCase() === "SCISSORS") {
-      result.textContent = "Scissors beat paper.";
-      setRoundWinner("Computer");
+    } else if (computerSelection.toUpperCase() === 'SCISSORS') {
+      result.textContent = 'Scissors beat paper.';
+      setRoundWinner('Computer');
       divComputerScore.textContent = `Computer's Score: ${++computerScore}`;
     }
   }
 
   //   If player chooses scissors
-  else if (playerSelection.toUpperCase() === "SCISSORS") {
-    if (computerSelection.toUpperCase() === "PAPER") {
-      result.textContent = "Scissors beat paper.";
-      setRoundWinner("Player");
+  else if (playerSelection.toUpperCase() === 'SCISSORS') {
+    if (computerSelection.toUpperCase() === 'PAPER') {
+      result.textContent = 'Scissors beat paper.';
+      setRoundWinner('Player');
       divPlayerScore.textContent = `Player's Score: ${++playerScore}`;
-    } else if (computerSelection.toUpperCase() === "ROCK") {
-      result.textContent = "Rock beats scissors.";
-      setRoundWinner("Computer");
+    } else if (computerSelection.toUpperCase() === 'ROCK') {
+      result.textContent = 'Rock beats scissors.';
+      setRoundWinner('Computer');
       divComputerScore.textContent = `Computer's Score: ${++computerScore}`;
     }
   }
@@ -80,7 +69,7 @@ function playRound(playerSelection, computerSelection) {
 
   // End round once any score reaches 5
   if (playerScore >= 5 || computerScore >= 5) {
-    playerScore >= 5 ? alert("Player wins!") : alert("Computer wins!");
+    playerScore >= 5 ? alert('Player wins!') : alert('Computer wins!');
     resetGame();
   }
 }
@@ -94,10 +83,11 @@ function resetGame() {
   computerScore = 0;
   divPlayerScore.textContent = "Player's Score: 0";
   divComputerScore.textContent = "Computer's Score: 0";
-  divPlayerSelection.textContent = "";
-  divComputerSelection.textContent = "";
-  result.textContent = "";
-  divRoundWinner.textContent = "";
+  divPlayerSelection.textContent = '';
+  divComputerSelection.textContent = '';
+  result.textContent = '';
+  divRoundWinner.textContent = '';
+  btnPlayRound.disabled = true;
 }
 
 // Write the final function called game()
@@ -117,13 +107,34 @@ function game() {
 
 // game();
 
-const btnRock = document.querySelector("#btn--rock");
-btnRock.addEventListener("click", () => playRound("ROCK", computerPlay()));
+let playerSelection;
 
-const btnPaper = document.querySelector("#btn--paper");
-btnPaper.addEventListener("click", () => playRound("PAPER", computerPlay()));
+const btnPlayRound = document.querySelector('#btn--play-round');
+btnPlayRound.disabled = true;
+btnPlayRound.addEventListener('click', () => {
+  try {
+    playRound(playerSelection, computerPlay());
+    btnPlayRound.disabled = true;
+  } catch (error) {
+    alert(error.message);
+  }
+});
 
-const btnScissors = document.querySelector("#btn--scissors");
-btnScissors.addEventListener("click", () =>
-  playRound("SCISSORS", computerPlay())
-);
+const btnRock = document.querySelector('#btn--rock');
+const btnPaper = document.querySelector('#btn--paper');
+const btnScissors = document.querySelector('#btn--scissors');
+
+const btnItems = [btnRock, btnPaper, btnScissors];
+const items = ['ROCK', 'PAPER', 'SCISSORS'];
+
+// Add event listeners to item buttons
+for (let i = 0; i < items.length; i++) {
+  setItemButtons(btnItems[i], items[i]);
+}
+
+function setItemButtons(btn, item) {
+  btn.addEventListener('click', () => {
+    playerSelection = item;
+    btnPlayRound.disabled = false;
+  });
+}
